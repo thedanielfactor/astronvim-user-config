453ceb57a8242c36edeed94ffc495bb9e2546600
Raymond Doran <raymond@thedanielfactor.com>
2023-12-04 15:17:50 -0600

    Updated Config

---
+++ plugins/harpoon.lua
@@ -0,0 +1,75 @@
+local prefix = "<leader>h"
+local term_string = vim.fn.exists("$TMUX") == 1 and "tmux" or "terminal"
+
+local icon = vim.g.icons_enabled and "󱡀 " or ""
+local utils = require("astronvim.utils")
+
+return {
+	{
+		"ThePrimeagen/harpoon",
+		config = function()
+			utils.set_mappings({
+				n = {
+					[prefix] = { desc = icon .. "Harpoon" },
+					[prefix .. "a"] = {
+						function()
+							require("harpoon.mark").add_file()
+						end,
+						desc = "Add file",
+					},
+					[prefix .. "e"] = {
+						function()
+							require("harpoon.ui").toggle_quick_menu()
+						end,
+						desc = "Toggle quick menu",
+					},
+					["<C-x>"] = {
+						function()
+							vim.ui.input({ prompt = "Harpoon mark index: " }, function(input)
+								local num = tonumber(input)
+								if num then
+									require("harpoon.ui").nav_file(num)
+								end
+							end)
+						end,
+						desc = "Goto index of mark",
+					},
+					["<C-p>"] = {
+						function()
+							require("harpoon.ui").nav_prev()
+						end,
+						desc = "Goto previous mark",
+					},
+					["<C-n>"] = {
+						function()
+							require("harpoon.ui").nav_next()
+						end,
+						desc = "Goto next mark",
+					},
+					[prefix .. "m"] = { "<cmd>Telescope harpoon marks<CR>", desc = "Show marks in Telescope" },
+					[prefix .. "t"] = {
+						function()
+							vim.ui.input({ prompt = term_string .. " window number: " }, function(input)
+								local num = tonumber(input)
+								if num then
+									require("harpoon." .. term_string).gotoTerminal(num)
+								end
+							end)
+						end,
+						desc = "Go to " .. term_string .. " window",
+					},
+				},
+			})
+		end,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+		cmd = { "Harpoon" },
+	},
+	{
+		"catppuccin/nvim",
+		optional = true,
+		opts = { integrations = { harpoon = true } },
+	},
+}
--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,9 +1,25 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    -- add more things to the ensure_installed table protecting against community packs modifying it
-    opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-      -- "lua"
-    })
-  end,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      { "andymass/vim-matchup", init = function() vim.g.matchup_matchparen_deferred = 1 end },
+      {
+        "HiPhish/rainbow-delimiters.nvim",
+        opts = function()
+          return {
+            strategy = {
+              [""] = function()
+                if not vim.b.large_buf then return require("rainbow-delimiters").strategy.global end
+              end,
+            },
+          }
+        end,
+        config = function(_, opts) require "rainbow-delimiters.setup"(opts) end,
+      },
+    },
+    opts = {
+      auto_install = vim.fn.executable "tree-sitter" == 1,
+      matchup = { enable = true },
+    },
+  },
 }
